7de91c4a2da4a197d219741b0955b7d86166314a
Kyle Maxwell <kyle.maxwell@Kyle-Maxwells-MacBook-Pro.local>
2021-12-28 12:41:05 -0800

    [Pics] Axios async get to unsplash, took out the apikey

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
-    onSearchSubmit(term) {
-        console.log(term);
+    state = { images: [] };
+
+    onSearchSubmit = async (term) => {
+        const unsplashResponse = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID',
+            },
+        });
+        this.setState({ images:unsplashResponse.data.results});
     }
 
     render () {
         return (
             <div className="ui container" style={{ marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         );
     }
