bc6e006a158d46fe1109eade08e8f8d6b1af559e
Kyle Maxwell <kyle.maxwell@Kyle-Maxwells-MacBook-Pro.local>
2021-12-29 11:09:37 -0800

    [Pics] Removed]

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com/',
     headers: {
-        Authorization: 'Client-ID r8r0vjfrs2f_UvY2_M93c0G_ir_ey0ndq8ZJJonbXis',
+        Authorization: 'Client-ID',
     },
 });
